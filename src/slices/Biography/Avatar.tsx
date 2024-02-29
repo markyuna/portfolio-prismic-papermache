80dee517d503f3b4d2236aae2ebbc87b464dda53
MARCOS <marcossuarezr88@gmail.com>
2024-02-29 22:09:00 +0100

    Avatar a propose gooD

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -80,8 +80,8 @@ export default function Avatar({
       >
         <PrismicNextImage
           field={image}
-          className="avatar-image h-full w-full object-fill"
-          imgixParams={{ q: 80 }}
+          className="avatar-image w-full object-fill"
+          imgixParams={{ q: 90 }}
         />
         <div className="highlight absolute inset-0 hidden w-full scale-110 bg-gradient-to-tr from-transparent via-white to-transparent opacity-0 md:block"></div>
       </div>
--- src/slices/ImageBlock/index.tsx
+++ src/slices/ImageBlock/index.tsx
@@ -16,7 +16,7 @@ const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
     <PrismicNextImage
       field={slice.primary.image}
       imgixParams={{ w: 300 }}
-      className="not-prose w-full h-full rounded-md  my-10 md:my-14 lg:my-16"
+      className="not-prose w-full h-full rounded-md my-10 md:my-14 lg:my-16"
     />
   );
 };
