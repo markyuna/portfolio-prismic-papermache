0c024e682b60c3c0117b07ee6df59388f727c72b
MARCOS <marcossuarezr88@gmail.com>
2024-05-23 21:49:08 +0200

    footer gooD

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default async function Footer() {
   const settings = await client.getSingle("settings");
   return (
     <Bounded as="footer" className="text-slate-600">
-      <div className="container mx-auto mt-20 flex flex-col items-center justify-between gap-6 py-8 sm:flex-row ">
+      <div className="container mx-auto mt-20 flex flex-col items-center justify-between gap-5 py-8 lg:flex-row">
         <div className="name flex flex-col items-center justify-center gap-x-4 gap-y-2 sm:flex-row sm:justify-self-start">
           <Link
             href="/"
@@ -21,23 +21,23 @@ export default async function Footer() {
             {settings.data.name}
           </Link>
           <span
-            className="hidden text-5xl font-extralight leading-[0] text-slate-400 sm:inline"
+            className="hidden text-2xl font-extralight leading-[0] text-slate-400 sm:inline"
             aria-hidden={true}
           >
             /
           </span>
-          <p className=" text-sm text-slate-300 ">
+          <p className="text-sm text-slate-300">
             © {new Date().getFullYear()} {settings.data.name}
           </p>
         </div>
-        <nav className="navigation" aria-label="Footer Navigation">
-          <ul className="flex items-center gap-1">
+        <nav className="navigation mt-4 sm:mt-0" aria-label="Footer Navigation">
+          <ul className="flex flex-wrap justify-center gap-2 sm:justify-start">
             {settings.data.nav_items.map(({ link, label }, index) => (
               <React.Fragment key={label}>
                 <li>
                   <PrismicNextLink
                     className={clsx(
-                      "group relative block overflow-hidden  rounded px-3 py-1 text-base font-bold text-slate-100 transition-colors duration-150 hover:hover:text-yellow-400",
+                      "group relative block overflow-hidden rounded px-3 py-1 text-base font-bold text-slate-100 transition-colors duration-150 hover:text-yellow-400",
                     )}
                     field={link}
                   >
@@ -46,7 +46,7 @@ export default async function Footer() {
                 </li>
                 {index < settings.data.nav_items.length - 1 && (
                   <span
-                    className="text-4xl font-thin leading-[0] text-slate-400"
+                    className="mt-4 text-4xl font-thin leading-[0] text-slate-400"
                     aria-hidden="true"
                   >
                     /
@@ -56,7 +56,7 @@ export default async function Footer() {
             ))}
           </ul>
         </nav>
-        <div className="socials inline-flex justify-center sm:justify-end">
+        <div className="socials mt-4 inline-flex justify-center gap-4 sm:mt-0 sm:justify-end">
           {isFilled.link(settings.data.github_link) && (
             <PrismicNextLink
               field={settings.data.github_link}
@@ -70,7 +70,7 @@ export default async function Footer() {
             <PrismicNextLink
               field={settings.data.instagram_link}
               className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
-              aria-label={settings.data.name + " on Twitter"}
+              aria-label={settings.data.name + " on Instagram"}
             >
               <FaInstagram />
             </PrismicNextLink>
