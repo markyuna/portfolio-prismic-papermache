920f930fb1b890f624e522a84be91afac4cf9a1a
MARCOS <marcossuarezr88@gmail.com>
2024-09-10 15:06:37 +0200

    config and globals.css

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,30 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    reactRemoveProperties: { properties: ["^data-custom$"] },
+    styledComponents: true,
+    removeConsole: true,
+    relay: {
+      src: "./",
+      artifactDirectory: "./__tests__",
+      language: "typescript",
+      eagerEsModules: false,
+    },
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        pathname: "/**", // Se permiten todas las rutas bajo el dominio especificado
       },
     ],
   },
-  env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  experimental: {
+    esmExternals: true,
   },
-  swcMinify: true,
 };
 
 export default nextConfig;
